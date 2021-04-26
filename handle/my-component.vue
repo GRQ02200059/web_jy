a6d695f3604b4f00b31e33c6070dfeac054771a4
lannister <lannister@qq.com>
2021-04-26 13:32:45 +0800

    11
    111

---
+++ handle/my-component.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="hello">Hello {{who}}</div>
+</template>
+
+<script type="module">
+const SeeksRelationGraph = require('relation-graph');
+module.exports = {
+    data: function() {
+        return {
+            who: 'world'
+        }
+    }
+}
+</script>
+
+<style>
+.hello {
+    background-color: #ffe;
+}
+</style>
